2e48daaa31e2f650decba05f2a1f2ec2be55803d
Tekina <nm0321@yandex.ru>
2024-11-08 17:33:17 +0300

    minor update x3

---
+++ commands/debugs.ts
@@ -0,0 +1,31 @@
+import SlashCommand from "../structures/Command";
+import {Client, ChatInputCommandInteraction, SlashCommandBuilder,} from "discord.js";
+import { primaryEmbed} from "../utils/embeds";
+import db from "../structures/Db";
+import {discordLogger} from "../utils/logger";
+
+export default class ExampleCommand extends SlashCommand {
+    constructor() {
+        super("debugs", "Техническая команда", {
+            requiredPermissions: [
+                'Administrator'
+            ]
+        })
+    }
+
+    async exec(interaction: ChatInputCommandInteraction) {
+        const test = await db.pool('SELECT * FROM bot_character', [], true)
+        const result = test.map((el) => String(el)).join(' | ')
+        discordLogger.error(result)
+
+        await interaction.reply({
+            embeds: [
+                primaryEmbed('Готово', `Дебагг про серверу проведен, данные в логах`, "Gold")
+            ], ephemeral: true
+        });
+    }
+
+    build(client: Client<boolean>, defaultCommand: SlashCommandBuilder) {
+        return defaultCommand.toJSON();
+    }
+}
--- commands/post.ts
+++ commands/post.ts
@@ -24,7 +24,7 @@ export default class ExampleCommand extends SlashCommand {
         await interaction.reply({
             embeds: [
                 primaryEmbed('Готово', `Пост опубликован`, "Gold")
-            ]
+            ], ephemeral: true
         });
     }
 
